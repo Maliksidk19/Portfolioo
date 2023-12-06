1c98f1edd83d1611d9c92c9bcf9561e451096342
Muhammad Saad <malik.saadking19@yahoo.com>
2023-12-06 22:58:44 +0500

    update education

--- components/Education.tsx
+++ components/Education.tsx
@@ -3,13 +3,25 @@ const Education = () => {
     <main className="w-100 flex flex-col gap-8">
       <h1 className="text-4xl font-semibold">Education</h1>
       <section className="grid grid-cols-2 gap-6">
-        <section className="flex gap-6">
+        <section className="flex gap-6 col-span-2">
           <span className="flex-none w-2 bg-teal-700 rounded-full grid place-items-center"></span>
           <div className="flex flex-col gap-2 flex-1">
             <h2 className="text-3xl">BS Computer Science</h2>
             <h3 className="text-xl text-[--textSoft]">University Of Haripur, Haripur</h3>
             <span>Oct 2021 - Present</span>
-            <p className="text-lg text-justify">Studying Computer Science</p>
+            <section className="flex flex-wrap gap-5 items-center mt-4">
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">DBMS</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">MySQL</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Operating System</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Networking</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Data Structure & Algorithm</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">C++</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Object Oriented Programming</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Web Engineering</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Digital Logic & Design</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Communication Skills</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Java</span>
+            </section>
           </div>
         </section>
         <section className="flex gap-6">
@@ -18,7 +30,11 @@ const Education = () => {
             <h2 className="text-3xl">FSC Pre Engineering</h2>
             <h3 className="text-xl text-[--textSoft]">Askaria College, Wah</h3>
             <span>Sept 2019 - May 2021</span>
-            <p className="text-lg text-justify">Studied Mathematics, Physics and Chemistry</p>
+            <section className="flex flex-wrap gap-5 items-center mt-4">
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Mathematics</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Physics</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Chemistry</span>
+            </section>
           </div>
         </section>
         <section className="flex gap-6">
@@ -27,7 +43,12 @@ const Education = () => {
             <h2 className="text-3xl">Matriculation</h2>
             <h3 className="text-xl text-[--textSoft]">Askaria College, Wah</h3>
             <span>Aug 2017 - Apr 2019</span>
-            <p className="text-lg text-justify">Studied Mathematics, Physics, Chemistry & Computer Science</p>
+            <section className="flex flex-wrap gap-5 items-center mt-4">
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Mathematics</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Physics</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Chemistry</span>
+              <span className="border-2 border-teal-700 text-2xl p-2 px-4 rounded-md text-center">Computer Science</span>
+            </section>
           </div>
         </section>
       </section>
